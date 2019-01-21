3273926cc221b2b5fa370179f3e63c90081eaa45
Sam L <45187486+samlafell@users.noreply.github.com>
2019-01-20 22:12:41 -0500

    Update Summation.py

--- Summation.py
+++ Summation.py
@@ -1,12 +1,12 @@
-num = input(type in a number:)
+num = input('type any number: ')
 
 addition = 0
 
-if num < 0:
+if int(num) < 0:
     print('make it positive, fam')
-elif num == 0:
+elif int(num) == 0:
     print('cant be 0')
-else:
-    for i in range(1,num+1):
+elif int(num) > 0:
+    for i in range(1,int(num)+1):
         addition = addition+i
-    print('Adding ' + str(num) + ' by every number below it = ' + str(addition))
+    print('Adding ' + str(int(num)) + ' by ever number below it = ' + str(addition))
